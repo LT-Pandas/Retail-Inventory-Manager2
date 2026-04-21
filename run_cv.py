fb5bafbcd81f26f87607a88c1f3e8ea16897e4a5
LTPandas <john.payton747@gmail.com>
2026-04-21 13:23:37 -0400

    ass

--- run_cv.py
+++ run_cv.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import time
 
 from cv_modular import CVPipeline, run_webcam_loop
 from cv_modular.finger_serial import FingerSerialSender, FingerSerialSenderConfig
